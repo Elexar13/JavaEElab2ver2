ae727c5fd2d6d9b92a2748e08be0a1d3acf5953b
Elexar13 <netnotkam@gmail.com>
2020-10-11 22:19:43 +0300

    Last commit

--- o0710/src/main/java/exceptions/CantFindCountryException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-import java.sql.SQLException;
-
-public class CantFindCountryException extends RuntimeException {
-    public CantFindCountryException(SQLException e) {
-        super(e);
-    }
-}
